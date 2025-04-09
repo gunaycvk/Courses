c8a43b41e4060acab0dffb03374e8a4ea64eca01
günay çevik <you@examplesdacm>
2025-04-09 05:18:26 +0300

    öğrencilerin listelenmesi için index sayfası oluşturuldu ve layout düzenlendi

--- Controllers/OgrenciController.cs
+++ Controllers/OgrenciController.cs
@@ -1,4 +1,7 @@
+using kurslar.Data;
+using Kurslar.Data;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Kurslar.Controllers
 {
@@ -6,10 +9,31 @@ namespace Kurslar.Controllers
     public class OgrenciController : Controller
     {
 
+        private readonly DataContext _context;
 
+        public OgrenciController(DataContext context)
+        {
+            _context = context;
+        }
+
+
+
+        public async Task<IActionResult> Index()
+        {
+            var ogrenciler = await _context.Ogrenciler.ToListAsync();
+            return View(ogrenciler);
+        }
         public IActionResult Create()
         {
             return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(Ogrenci model)
+        {
+            _context.Ogrenciler.Add(model);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index", "Home");
+
         }
     }
 }
